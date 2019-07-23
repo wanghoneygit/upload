d2571c2bb2738f0320de5d8ce1e2695a4dce8902
wuwangwang <wuwangwang@dxy.cn>
2019-07-23 21:40:11 +0800

    新增 包基本文件

---
+++ src/Upload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wang
+ * Date: 2019/7/23
+ * Time: 下午9:33
+ */
+
+class Upload {
+
+    public function getTest() {
+        echo 'Hello World';
+    }
+}
